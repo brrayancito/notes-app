dcfc84649e3c052e13f465da079faf1ce106da69
Brrayan Javy <brrayanperez10@gmail.com>
2023-06-23 18:07:37 -0400

    Displaying notes and pagination

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -1,17 +1,46 @@
+const mongoose = require('mongoose');
+
 const Note = require('../models/noteModel.js');
 
 // Get Dashboard
 exports.dashboard = async (req, res) => {
+  let perPage = 12;
+  let page = req.query.page || 1;
+
   const locals = {
     title: 'Dashboard',
     description: 'Free NodeJs Notes App',
   };
 
-  res.render('dashboard/index', {
-    userName: req.user.firstName,
-    locals,
-    layout: '../views/layouts/dashboard.ejs',
-  });
+  try {
+    // Mongoose "^7.0.0 Update
+    const notes = await Note.aggregate([
+      { $sort: { updatedAt: -1 } },
+      { $match: { user: new mongoose.Types.ObjectId(req.user.id) } },
+      {
+        $project: {
+          title: { $substr: ['$title', 0, 30] },
+          body: { $substr: ['$body', 0, 100] },
+        },
+      },
+    ])
+      .skip(perPage * page - perPage)
+      .limit(perPage)
+      .exec();
+
+    const count = await Note.count();
+
+    res.render('dashboard/index', {
+      userName: req.user.firstName,
+      locals,
+      notes,
+      layout: '../views/layouts/dashboard',
+      current: page,
+      pages: Math.ceil(count / perPage),
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 // Notes data test
